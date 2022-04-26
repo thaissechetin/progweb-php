02263599bc543ec9bd1388c48ec44a72528fbfe5
Thaís Sechetin <sechetin.thais@gmail.com>
2022-04-26 15:52:42 -0300

    Exercício 1

---
+++ 03-exercicios.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercícios</title>
+</head>
+<body>
+    <h1>Exercício 1:</h1>
+
+    <?php
+
+    $data = " 26/04/2022";
+    $nome1 = "Thaís Sechetin";
+    $idade = 34;
+    $cidade = "São Paulo";
+
+    ?>
+
+   <p>Hoje é dia <?=$data?>.<?=$nome1?> tem <?=$idade?> anos e mora em <?=$cidade?>. </p>
+
+</body>
+</html>
