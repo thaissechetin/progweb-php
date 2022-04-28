5af3cfac0e5abe3960481ba4edc7d7c0462108da
Thaís Sechetin <sechetin.thais@gmail.com>
2022-04-28 14:31:11 -0300

    Correção do exercício 03

--- exercicio03.php
+++ exercicio03.php
@@ -10,25 +10,45 @@
     <h1>Exercício 03:</h1>
 
     <?php
-        $salarioAtual = 500;
-        
-
+        $salario = 15000;
+        $reajuste = ""; // Declarar a variável inicialmente é algo opcional em PHP. Em JS é obrigatório declarar.
     
-     if ($salarioAtual< 500) {
-         $reajuste = $salarioAtual * 1.15;
-     } if ($salarioAtual <= 1000) {
-         $reajuste = $salarioAtual *1.10;
+
+     if ($salario < 500) {
+         $reajuste = $salario * 1.15;
+     } elseif ($salario <= 1000) {
+         $reajuste = $salario *1.10;
      } else {
-        $reajuste = $salarioAtual * 1.05;
+        $reajuste = $salario * 1.05;
      }
 
+    
      
-     echo "<p> O salário atual é $salarioAtual.</p>";
-     echo "<p>O salário com reajuste é de $reajuste.</p>"
+     echo "<p> O salário antes do reajuste é $salario. O salário reajustado é $reajuste.</p>";
+     //ou
+
+     ?>
+
+
+     <p>O salário antigo é R$<?=number_format($salario, 2 , ",", ".")?>. 
+        O salário atual é de R$<?=number_format($reajuste, 2 , ",", ".")?>.</p>
+     <!-- Esta forma é mais recomendada como boa prática. -->
+    
+
+     <!-- 
+         number_format (
+             valor,
+             quantidade de casas decimais,
+             "separador de casa decimal", 
+             "separador de milhar"
+         )
+      -->
+     
+     
      
 
     
-    ?>
+    
 
 
 </body>
